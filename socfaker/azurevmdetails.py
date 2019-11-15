216a17c3629787447ec58d5d86fa420c2cde4786
Josh Rickard <josh.rickard@swimlane.com>
2019-11-15 11:57:28 -0600

    Adding AzureVM classes

---
+++ socfaker/azurevmdetails.py
@@ -0,0 +1,144 @@
+import uuid, random
+from computer import Computer
+from words import Words
+
+
+__SIZE_LIST__ = [
+  'Standard_B1ls1',
+  'Standard_B1s',
+  'Standard_B1ms',
+  'Standard_B2s',
+  'Standard_B2ms',
+  'Standard_B8ms',
+  'Standard_B4ms',
+  'Standard_B12ms',
+  'Standard_B16ms',
+  'Standard_B20ms'
+]
+
+__LOCATION_LIST__ = [
+  'eastasia',
+  'southeastasia',
+  'centralus',
+  'eastus',
+  'eastus2',
+  'westus',
+  'northcentralus',
+  'southcentralus',
+  'northeurope',
+  'westeurope',
+  'japanwest',
+  'japaneast',
+  'brazilsouth',
+  'australiaeast',
+  'australiasoutheast',
+  'southindia',
+  'centralindia',
+  'westindia',
+  'canadacentral',
+  'canadaeast',
+  'uksouth',
+  'ukwest',
+  'westcentralus',
+  'westus2',
+  'koreacentral',
+  'koreasouth',
+  'francecentral',
+  'francesouth',
+  'australiacentral',
+  'australiacentral2',
+  'southafricanorth',
+  'southafricawest'
+]
+class AzureVM(object):
+
+    def __init__(self):
+        self.word_list = Words().get()
+
+    def __get_unique_ids(self):
+        return_dict = {}
+        return_dict['subscription_id'] = str(uuid.uuid4())
+        return_dict['vm_id'] = str(uuid.uuid4())
+        return return_dict
+
+    def __os_type(self):
+        return_dict = {}
+        os_type = random.choice(['Windows', 'RedHat', 'CentOS'])
+        if os_type == 'Windows':
+            return_dict['os'] = os_type
+            return_dict['publisher'] = random.choice(['MicrosoftWindowsDesktop', 'MicrosoftWindowsServer'])
+            return_dict['offer'] = "Windows-{}".format(random.choice(['10', '7', '8']))
+            return_dict['license'] = random.choice(['Windows_Client', 'Windows_Server'])
+        else:
+            return_dict['os'] = os_type
+            return_dict['publisher'] = random.choice(['OpenLogic', 'RedHat', 'Canonical'])
+            return_dict['offer'] = random.choice(['CentOS-7.5', 'Debian-8'])
+            return_dict['license'] = random.choice(['Linux_Client', 'Linux_Server'])
+        return return_dict
+
+    def generate(self):
+        unique_ids = self.__get_unique_ids()
+        os_type = self.__os_type()
+        return_dict = {}
+        computer_name = Computer().name
+        resource_group_name = '{}_{}_{}'.format(random.choice(self.word_list), random.choice(self.word_list), random.choice(self.word_list))
+        os_disk_name = "{}_OsDisk_1_{}".format(computer_name, uuid.uuid4().hex)
+        return_dict['name'] = computer_name
+        return_dict['id'] = "/subscriptions/{sub_id}/resourceGroups/{rgn}/providers/Microsoft.Compute/virtualMachines/{name}".format(
+            sub_id=unique_ids['subscription_id'],
+            rgn=resource_group_name,
+            name=computer_name
+        )
+        return_dict['type'] = 'Microsoft.Compute/virtualMachines'
+        return_dict['location'] = random.choice(__LOCATION_LIST__)
+        return_dict['properties'] = {
+          'vmId': unique_ids['vm_id'],
+          'hardwareProfile': {
+            'vmSize': random.choice(__SIZE_LIST__)
+          }
+        }
+        return_dict['storageProfile'] = {
+          "imageReference": {
+            "publisher": os_type['publisher'],
+            "offer": os_type['offer'],
+            "sku": "rs5-pro",
+            "version": "latest"
+          },
+          "osDisk": {
+            "osType": os_type['os'],
+            "name": os_disk_name,
+            "createOption": "FromImage",
+            "caching": "ReadWrite",
+            "managedDisk": {
+              "storageAccountType": "Premium_LRS",
+              "id": "/subscriptions/{sub_id}/resourceGroups/{rgn}/providers/Microsoft.Compute/disks/{disk}".format(sub_id=unique_ids['subscription_id'], rgn=resource_group_name, disk=os_disk_name)
+            },
+            "diskSizeGB": 127
+          },
+          "dataDisks": []
+        }
+        return_dict['osProfile'] = {
+          "computerName": computer_name,
+          "adminUsername": "swimlane",
+          "windowsConfiguration": {
+            "provisionVMAgent": random.choice(['true', 'false']),
+            "enableAutomaticUpdates": random.choice(['true', 'false'])
+          },
+          "secrets": [],
+          "allowExtensionOperations": random.choice(['true', 'false']),
+          "requireGuestProvisionSignal": random.choice(['true', 'false'])
+        }
+        return_dict['networkProfile'] = {
+          'networkInterfaces': [{
+            'id': "/subscriptions/{sub_id}/resourceGroups/{rgn}/providers/Microsoft.Network/networkInterfaces/{name}{count}".format(sub_id=unique_ids['subscription_id'], rgn=resource_group_name, name=computer_name, count=random.randint(500,1000))
+          }]
+        }
+        return_dict['diagnosticsProfile'] = {
+          "bootDiagnostics": {
+            "enabled": random.choice(['true', 'false']),
+            "storageUri": "https://{rgn}dia125.blob.core.windows.net/".format(rgn=resource_group_name.replace('_',''))
+          }
+        }
+        return_dict['licenseType'] = os_type['license']
+        return_dict['provisioningState'] = 'Succeeded'
+        return return_dict
---
+++ socfaker/azurevmmetrics.py
@@ -0,0 +1,76 @@
+import pendulum, random
+
+__METRIC_LIST__ = {
+    'percent': [
+        'percentage_cpu',
+        'premium_data_disk_cache_read_hit',
+        'premium_data_disk_cache_read_miss',
+        'premium_os_disk_cache_read_hit',
+        'premium_os_disk_cache_read_miss',
+    ],
+    'bytes': [
+        'network_in_billable_deprecated',
+        'network_out_billable_deprecated',
+        'disk_read_bytes',
+        'disk_write_bytes',
+        'network_in_total',
+        'network_out_total'
+    ],
+    'count_per_second': [
+        'disk_read_operations_sec',
+        'inbound_flows_maximum_creation_rate',
+        'outbound_flows_maximum_creation_rate',
+        'disk_write_operations_sec',
+        'os_disk_write_bytes_sec',
+        'os_disk_read_operations_sec',
+        'data_disk_read_bytes_sec_deprecated',
+        'data_disk_write_bytes_sec_deprecated',
+        'data_disk_read_operations_sec_deprecated',
+        'data_disk_write_operations_sec_deprecated',
+        'data_disk_read_operations_sec',
+        'data_disk_write_operations_sec',
+        'os_disk_read_bytes_sec',
+        'os_disk_write_operations_sec'
+    ],
+    'count': [
+        'cpu_credits_remaining',
+        'cpu_credits_consumed',
+        'data_disk_qd_deprecated',
+        'inbound_flows',
+        'outbound_flows',
+        'os_disk_qd_deprecated',
+        'os_disk_queue_depth',
+        'data_disk_queue_depth'
+    ]
+}
+
+class AzureVMMetrics(object):
+
+    def get(self):
+        metric_dict = {}
+        yesterday = pendulum.yesterday()
+        now = pendulum.now()
+        for key, val in __METRIC_LIST__.items():
+            for metric in val:
+                time_stamp = yesterday
+                timestamp_list = []
+                while time_stamp <= now:
+                    total_value = None
+                    if key == 'percent':
+                        total_value = round(float(random.randint(0, 100)),2)
+                    elif key == 'bytes':
+                        total_value = round(float(random.randint(50, 10000)),2)
+                    elif key == 'count_per_second':
+                        total_value = round(float(random.randint(50, 1000)), 2)
+                    elif key == 'count':
+                        total_value = round(float(random.uniform(0.0, 1000)), 2)
+                    else:
+                        pass
+                    time_stamp = time_stamp.add(hours=1)
+                    timestamp_list.append({
+                            'time_stamp': time_stamp.to_iso8601_string(),
+                            'total': total_value,
+                            'unit': key
+                        })
+                metric_dict[metric] = timestamp_list
+        return metric_dict
