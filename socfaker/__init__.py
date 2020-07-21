72d8e345b181233230c5d261b4f934e9e4f6953b
Josh Rickard <josh.rickard@MacBook-Pro.local>
2020-07-21 12:19:52 -0500

    modified __init__

--- socfaker/__init__.py
+++ socfaker/__init__.py
@@ -1,11 +1,1 @@
 from .socfaker import SocFaker
-
-#from .vulnerability import Vulnerability
-#from .application import Application
-#from .computer import Computer
-#from .employee import Employee
-#from .file import File
-#from .network import Network
-#from .organization import Organization
-#from .vulnerabilityhost import VulnerabilityHost
-#from .vulnerabilityscan import VulnerabilityScan
