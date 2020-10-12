69ee34e68e93284db928d5760702d8c444587aae
Josh Rickard <josh.rickard@ip-192-168-1-2.us-west-2.compute.internal>
2020-10-12 11:46:40 -0500

    expanded additional tests

--- tests/test_socfaker_agent.py
+++ tests/test_socfaker_agent.py
@@ -1,14 +1,20 @@
 def test_socfaker_agent_ephermal_id(socfaker_fixture):
-    assert socfaker_fixture.agent.ephermeral_id
+    assert len(socfaker_fixture.agent.ephermeral_id) == 8
+
+def test_socfaker_agent_ephermal_id_not_duplicate(socfaker_fixture):
+    assert socfaker_fixture.agent.ephermeral_id != socfaker_fixture.agent.ephermeral_id
 
 def test_socfaker_agent_id(socfaker_fixture):
-    assert socfaker_fixture.agent.id
+    assert len(socfaker_fixture.agent.id) == 8
+
+def test_socfaker_agent_id_not_duplicate(socfaker_fixture):
+    assert socfaker_fixture.agent.id != socfaker_fixture.agent.id
 
 def test_socfaker_agent_name(socfaker_fixture):
-    assert socfaker_fixture.agent.name
+    assert str(socfaker_fixture.agent.name).split('-')[0] in ['Desktop', 'Laptop']
 
 def test_socfaker_agent_type(socfaker_fixture):
-    assert socfaker_fixture.agent.type
+    assert socfaker_fixture.agent.type in ['filebeat', 'auditbeat', 'functionbeat', 'heartbeat', 'winlogbeat', 'packetbeat']
 
 def test_socfaker_agent_version(socfaker_fixture):
-    assert socfaker_fixture.agent.version
+    assert socfaker_fixture.agent.version == '7.8.0'
