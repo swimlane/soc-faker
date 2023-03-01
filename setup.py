d9384b357ecd31ecf3aad89ce7e19a9b2ecae295
dselig11235 <daniel.selig@swimlane.com>
2023-02-28 18:45:19 -0600

    build: Version bump for release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def parse_requirements(requirement_file):
 
 setup(
     name='soc-faker',
-    version='4.0.1',
+    version='4.0.2',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='A Python package to fake SOC (Security Operations Center) data',
